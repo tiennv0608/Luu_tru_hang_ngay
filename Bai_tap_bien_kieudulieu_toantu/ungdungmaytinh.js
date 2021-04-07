01235a959c5c8665ce305e439df580e550c72390
tiennv0608 <tiennv0608@gmail.com>
2021-04-07 13:48:25 +0700

    .

---
+++ Bai_tap_bien_kieudulieu_toantu/ungdungmaytinh.js
@@ -0,0 +1,11 @@
+function clickButton(){
+    let firstNumber = document.getElementById('firstNumber').value;
+    let secondNumber = document.getElementById('secondNumber').value;
+    let a = parseInt(firstNumber);
+    let b = parseInt(secondNumber);
+    let add = document.getElementById('add').text;
+    let sub = document.getElementById('sub').text;
+    let mul = document.getElementById('mul').text;
+    let div = document.getElementById('div').text;
+
+}
