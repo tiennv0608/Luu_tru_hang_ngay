2d56479f66626c091e4601872d0a4531d07be61c
tiennv0608 <tiennv0608@gmail.com>
2021-04-07 21:26:42 +0700

    btcautrucdieukien

---
+++ Bai_tap_bien_kieudulieu_toantu/ungdungmaytinh2.js
@@ -0,0 +1,11 @@
+function clickButton() {
+    let firstNumber = document.getElementById('firstNumber').value;
+    let a = parseFloat(firstNumber);
+    let secondNumber = document.getElementById('secondNumber').value;
+    let b = parseFloat(secondNumber);
+    let v;
+    let add = document.getElementById("add").value;
+    let sub = document.getElementById("sub").value;
+    let mul = document.getElementById("mul").value;
+    let div = document.getElementById("div").value;
+    if onclick(document.getElementById('add'))
